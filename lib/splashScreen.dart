f62bc22a9fd2502a541384ed7f58643df9ba283e
Rohit Ranjan <rohitgns54321@gmail.com>
2023-07-27 13:37:11 +0530

    splash screen and package

--- lib/splashScreen.dart
+++ lib/splashScreen.dart
@@ -1,6 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
-
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -9,17 +10,18 @@ class SplashScreen extends StatefulWidget {
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin{
-
-  late final AnimationController _controller = AnimationController(vsync: this,duration: const Duration(seconds: 3))..repeat();
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller =
+      AnimationController(vsync: this, duration: const Duration(seconds: 3))
+        ..repeat();
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-
-
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,10 +29,30 @@ class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMix
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children:  [
-            AnimatedBuilder(animation: _controller, builder: (BuildContext context,Widget? child){
-              return Transform.rotate(angle: _controller.value,child: ,)
-            })
+          children: [
+            AnimatedBuilder(
+                animation: _controller,
+                builder: (BuildContext context, Widget? child) {
+                  return Transform.rotate(
+                    angle: _controller.value * 2.0 * math.pi,
+                    child: Container(
+                      height: 200,
+                      width: 200,
+                      child: const Center(
+                        child: Image(
+                          image: AssetImage('assets/virus.png'),
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+            SizedBox(height: MediaQuery.of(context).size.height*0.8,),
+            const Align(
+              alignment: Alignment.center,
+              child: Text('Covid-19\nTracker App',style: TextStyle(
+                fontWeight: FontWeight.bold,fontSize: 25
+              ),),
+            )
           ],
         ),
       ),
