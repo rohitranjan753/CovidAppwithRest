518f2fa1722923ccb31c49607dd9313cd2d8db71
Rohit Ranjan <rohitgns54321@gmail.com>
2023-07-27 14:06:44 +0530

    splash screen and package

--- lib/world_states.dart
+++ lib/world_states.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pie_chart/pie_chart.dart';
+
 class WorldStatesScreen extends StatefulWidget {
   const WorldStatesScreen({Key? key}) : super(key: key);
 
@@ -7,11 +8,11 @@ class WorldStatesScreen extends StatefulWidget {
   State<WorldStatesScreen> createState() => _WorldStatesScreenState();
 }
 
-class _WorldStatesScreenState extends State<WorldStatesScreen> with TickerProviderStateMixin{
+class _WorldStatesScreenState extends State<WorldStatesScreen>
+    with TickerProviderStateMixin {
   late final AnimationController _controller =
-  AnimationController(vsync: this, duration: const Duration(seconds: 3))
-    ..repeat();
-
+      AnimationController(vsync: this, duration: const Duration(seconds: 3))
+        ..repeat();
 
   @override
   void dispose() {
@@ -19,7 +20,7 @@ class _WorldStatesScreenState extends State<WorldStatesScreen> with TickerProvid
     _controller.dispose();
   }
 
-  final colorList=<Color> [
+  final colorList = <Color>[
     const Color(0xff4285F4),
     const Color(0xff1aa260),
     const Color(0xffde5246),
@@ -33,9 +34,18 @@ class _WorldStatesScreenState extends State<WorldStatesScreen> with TickerProvid
           padding: const EdgeInsets.all(15.0),
           child: Column(
             children: [
-              SizedBox(height: MediaQuery.of(context).size.height*0.01,),
-              PieChart(dataMap: {"Total":20,"Recovered": 15,"Deaths":5},animationDuration:const Duration(milliseconds: 1200),)
-
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.01,
+              ),
+              PieChart(
+                dataMap: {"Total": 20, "Recovered": 15, "Deaths": 5},
+                animationDuration: const Duration(milliseconds: 1200),
+                chartType: ChartType.ring,
+                colorList: colorList,
+                legendOptions: const LegendOptions(
+                  legendPosition: LegendPosition.left
+                ),
+              )
             ],
           ),
         ),
